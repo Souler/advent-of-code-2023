f64d349379f81f76f31d5917569401e1ff51a20b
Souler <juanjoseherrero@playtomic.io>
2023-12-03 12:09:25 +0100

    feat: day03 parts 1 and 2

---
+++ day03/part2.js
@@ -0,0 +1,80 @@
+// https://adventofcode.com/2023/day/3
+
+import { readInput as _readInput } from '../utils.js'
+
+const schematic = readInput()
+
+const result = schematic.reduce((acc, part) => {
+  if (part.value !== '*') {
+    return acc
+  }
+
+  const neighbors = findNeighbors(schematic, part)
+  const partNumberNeighbors = neighbors.filter(neighbor => isNumeric(neighbor.value))
+
+  if (partNumberNeighbors.length !== 2) {
+    return acc
+  }
+
+  const [part1, part2] = partNumberNeighbors
+  const gearRatio = Number.parseInt(part1.value) * Number.parseInt(part2.value)
+
+  return acc + gearRatio
+}, 0)
+
+console.log('>> result', result)
+
+// ---
+
+function readInput() {
+  const things = _readInput(import.meta.url, (line, y) => {
+    const lineParts = []
+    
+    const rgx = /([0-9]+|[^\.])/g
+    let match = rgx.exec(line)
+    while (match) {
+      lineParts.push({ x: match.index, y, value: match[1] })
+      match = rgx.exec(line)
+    }
+
+    return lineParts
+  })
+
+  return things.flat()
+}
+
+function isNumeric(value) {
+  return /^([0-9]+)$/.test(value)
+}
+
+function neighborBox(part) {
+  return {
+    x1: part.x - 1,
+    y1: part.y - 1,
+    x2: part.x + part.value.length,
+    y2: part.y + 1,
+  }
+}
+
+function boxOverlap(boxA, boxB) {
+	// no horizontal overlap
+	if (boxA.x1 >= boxB.x2 || boxB.x1 >= boxA.x2) return false;
+
+	// no vertical overlap
+	if (boxA.y1 >= boxB.y2 || boxB.y1 >= boxA.y2) return false;
+
+	return true;
+}
+
+function findNeighbors(parts, part) {
+  return parts.filter(otherPart => {
+    if (otherPart === part) {
+      return false
+    }
+
+    const partBox = neighborBox(part)
+    const otherPartBox = neighborBox(otherPart)
+    
+    return boxOverlap(partBox, otherPartBox)
+  })
+}
